87938d80e34a48ac2dfb7c0c0a1fc0643eb8da7c
jona.loeffler <jona.loeffler@homepagelabs.com>
2018-12-01 18:55:23 +0100

    Initial deadlines commit

---
+++ src/components/Deadline.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>{{remaining}}</li>
+</template>
+
+<script>
+export default {
+  props: {
+    deadline: {
+      type: Number,
+      required: true,
+      default: 12
+    }
+  },
+  data() {
+    return {
+      now: Math.trunc(new Date().getTime() / 1000)
+    };
+  },
+  mounted() {
+    window.setInterval(() => {
+      this.now = Math.trunc(new Date().getTime() / 1000);
+    });
+  },
+  computed: {
+    remaining: function() {
+      return new Date(this.deadline - this.now * 1000).toLocaleString();
+    }
+  }
+};
+</script>
---
+++ src/components/DeadlineList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <Deadline :deadline="deadline" v-for="deadline in deadlines" v-bind:key="deadline"></Deadline>
+  </ul>
+</template>
+
+<script>
+import Deadline from "./Deadline";
+export default {
+  components: {
+    Deadline
+  },
+  data() {
+    return {
+      deadlines: [1543686967656]
+    };
+  }
+};
+</script>
+
