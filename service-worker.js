9ca06a49d7d6c4f1964e1a0366323bcbee2ac31f
Jona Loeffler <j.loeffler@flybytes.de>
2020-05-02 20:34:25 +0200

    deploy

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+importScripts("/deadlines/precache-manifest.fbb1cd44848df37b4af0a9ceef24334d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.setConfig({
+    debug: true,
+});
+
+workbox.core.setCacheNameDetails({
+    prefix: 'deadlines',
+    suffix: 'v0.1.2'
+  });
+
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'images',
+    }),
+);
+
+workbox.routing.registerRoute(
+    /\.(?:ttf|woff|woff2)$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'fonts',
+    }),
+);
+
+workbox.routing.registerRoute(
+    new RegExp('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'bootstrap',
+    }),
+);
+workbox.routing.registerRoute(
+    new RegExp('https://use.fontawesome.com/releases/v5.8.1/css/all.css'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'fontawesome',
+    }),
+);
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+
+
