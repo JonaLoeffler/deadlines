b9f6987e9046af0ad95bd25a8d0f2c240ac9f1ab
Jona Löffler <j.loeffler@flybytes.de>
2020-08-15 11:58:20 +0200

    Add icon source file

---
+++ resources/splash.sh
@@ -0,0 +1,8 @@
+npx pwa-asset-generator ./resources/icon.png ./public/img/splash/ \
+--padding "25%"  \
+--background "#2b2b2b" \
+--manifest ./public/manifest.json \
+--index ./public/index.html \
+--quality 90 \
+--dark-mode true \
+
